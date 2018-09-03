74aa14ba2a2a2bfd130cb461cb53be4bc1d4d463
Ricardo Koller <kollerr@us.ibm.com>
2018-09-03 18:34:57 -0400

    Adding test for mapped devices

--- test_btreplay.sh
+++ test_btreplay.sh
@@ -72,9 +72,31 @@ test_write_digest_per_sector_no_stalls() {
 	fi
 }
 
+# Test 5: check that the write below was replayed correctly (from mail.replay.0) on a mapped device:
+# The homes traces have a digest per 8 sectors, or 32 bytes per 1 page (the -l 32 in the args)
+#
+# 89967404311353 4253 nfsd 680 16 W 6 0 b5e9f4e5ab62a4fff5313a606b0ad4e3e6434714a2358bc5f55005d6c5502d80
+test_write_digest_per_page_no_stalls_mapped() {
+	sudo rm -f /opt/dev/mail
+	sudo mkdir -p /opt/dev
+	sudo truncate -s $((1000 * 512)) /opt/dev/mail
+	echo mail /opt/dev/mail > map
+	sudo ./btreplay/btreplay -M map -l 32 -N -W mail
+	s1=`cut -b $((680 * 512))-$((688 * 512)) /opt/dev/mail`
+	s2=`cut -b $((688 * 512))-$((696 * 512)) /opt/dev/mail`
+	if [ "b5e9f4e5ab62a4fff5313a606b0ad4e3" == "$s1" ] && \
+		[ "e6434714a2358bc5f55005d6c5502d80" == "$s2" ]; then
+		echo ok
+	else
+		echo "fail: $s2"
+		exit 1
+	fi
+}
+
 test_write_digest_per_page
 test_write_digest_per_page_no_stalls
 test_write_digest_per_sector
 test_write_digest_per_sector_no_stalls
+test_write_digest_per_page_no_stalls_mapped
 
 exit 0
